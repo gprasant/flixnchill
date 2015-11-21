e8ea28655b865443ee5962b802710e1f093a7ecb
Prasanth Guruprasad <nagu89@gmail.com>
2015-11-21 15:27:38 -0800

    add chatsViewController

---
+++ tinder/ChatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatsViewController.h
+//  tinder
+//
+//  Created by Prasanth Guruprasad on 11/21/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatsViewController : UIViewController
+
+@end
