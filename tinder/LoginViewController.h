7a817dbd0ee3583239d2d7797414c361f8151d1a
Prasanth Guruprasad <nagu89@gmail.com>
2015-11-21 16:39:49 -0800

    Add login screen

---
+++ tinder/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  tinder
+//
+//  Created by Prasanth Guruprasad on 11/21/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ tinder/LoginViewController.m
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.m
+//  tinder
+//
+//  Created by Prasanth Guruprasad on 11/21/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self setupViews];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void) setupViews {
+    self.loginButton.layer.borderWidth = 1.0f;
+    self.loginButton.layer.borderColor = [[UIColor blackColor] CGColor];
+}
+
+
+@end
